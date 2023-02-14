61c4cf05dfd30fc629ad994b01200115b09b97a9
MimoKassy96 <vlad.karpovich@outlook.com>
2023-02-14 16:25:52 +0300

    Change validation rule

--- Bank.Application/Transactions/CreateTransaction/CreateTrasactionValidation.cs
+++ Bank.Application/Transactions/CreateTransaction/CreateTrasactionValidation.cs
@@ -7,11 +7,13 @@ namespace Bank.Application.Transactions.CreateTransaction
     /// </summary>
     public class CreateTransactionValidation : AbstractValidator<CreateTransactionCommand>
     {
-        public CreateTransactionValidation() 
+        public CreateTransactionValidation()
         {
             RuleFor(x => x.CardNumber).MaximumLength(16).WithMessage("Credit card number is too long");
 
-            RuleFor(x => x.CardNumber).NotEmpty().Matches(@"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$");
+            RuleFor(x => x.CardNumber).NotEmpty().CreditCard().Matches(@"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\d{3})\\d{11})$").WithMessage("Wrong credit card number");
+
+            RuleFor(x => x.Sum).GreaterThanOrEqualTo(0.01);
         }
     }
 }
